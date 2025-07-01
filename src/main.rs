46b9e785356dd73dc0696fffe5b61e0a9451c533
delcin <delcinraj@gmail.com>
2025-07-01 15:25:37 +0530

    query param

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use nostr::Url;
 
 fn main() {
     let server = "https://nosto.re";
-    let server_url = Url::parse(server).unwrap();
-    
-    let url_str = format!("{}", server_url);
-    let ending_char = url_str.chars().last().unwrap();
+    let mut url = Url::parse(server).unwrap();
+    let mut qm = url.query_pairs_mut();
+    qm.append_pair("key1", "value1");
+    qm.append_pair("key2", "value2");
+    qm.append_pair("key3", "value3");
+    drop(qm);
 
-    // Trailing '/' is automatically added
-    assert_eq!(ending_char, '/');
-
-    println!("{}", format!("{}upload", server_url));
+    println!("{url}");
 }
